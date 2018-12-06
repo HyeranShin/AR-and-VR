384ebb8f3da3a3c17344aadeb9f5dc1df2018c2f
HyeranShin <hyebaragi__@naver.com>
2018-12-06 23:58:28 +0900

    Add 12.06 practices

---
+++ AR/LancekunGo/Assets/Scripts/CatchBall.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchBall : MonoBehaviour
+{
+
+    bool is_drag_now_ = false;
+    float catch_ball_distance_;
+    public float catch_ball_throw_speed_ = 120;
+    public float catch_ball_arch_speed_ = 100;
+    public float catch_ball_speed_ = 1000;
+
+    //마우스 단추를 누르는 동안을 처리하는 내장 함수. Input.GetMouseButtonDown 함수로 대체 가능
+    void OnMouseDown()
+    {
+        catch_ball_distance_ = Vector3.Distance(transform.position, Camera.main.transform.position);
+        is_drag_now_ = true;
+    }
+    //마우스 클릭이 해제되거나 터치가 떨어지는 것을 인식하여 처리하는 내장 함수.
+    void OnMouseUp()
+    {
+        //중력과 속도를 설정
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().velocity += transform.forward * catch_ball_throw_speed_;
+        GetComponent<Rigidbody>().velocity += transform.up * catch_ball_arch_speed_;
+        is_drag_now_ = false;
+    }
+
+    void Update()
+    {
+        if (is_drag_now_)
+        {
+            //카메라로부터 스크린의 점을 통해 레이를 반환. 즉, 마우스 또는 터치하는 위치로부터 카메라를 통해 광선을 쏨.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //광선을 따라 거리 단위로 가리킴. 교차 지점을 알아냄
+            Vector3 ray_point = ray.GetPoint(catch_ball_distance_);
+            //Vector3.Lerp : 오브젝트의 회전의 회전각을 주는 함수
+            transform.position = Vector3.Lerp(transform.position, ray_point, catch_ball_speed_ * Time.deltaTime);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ AR/LancekunGo/Assets/Scripts/Lancekun_Go_Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kudan.AR;
+
+public class Lancekun_Go_Main : MonoBehaviour {
+
+    public KudanTracker kudan_tracker_;
+
+	IEnumerable Start()
+    {
+        // 1초간 대기 시킴
+        yield return new WaitForSeconds(1.0f);
+
+        // 바닥의 위치 값과 바닥의 회전 값을 저장할 변수 선언. Quaternion(사원수)
+        Vector3 floor_position;
+        Quaternion floor_orientation;
+
+        // 현재 바닥의 위치와 방위 값을 얻어옴
+        kudan_tracker_.FloorPlaceGetPose(out floor_position, out floor_orientation);
+
+        // 얻어온 값을 이용하여 트래킹 시작함
+        kudan_tracker_.ArbiTrackStart(floor_position, floor_orientation);
+    }
+}
